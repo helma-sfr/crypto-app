d63e275ab2b1ff28eac2a3a820f85297bb823f6b
Your Name <100334633+r0ghaye@users.noreply.github.com>
2025-01-12 16:07:18 +0330

    worked on TableCoin component and its styles also added alias to the project

--- src/components/modules/TableCoin.jsx
+++ src/components/modules/TableCoin.jsx
@@ -1,26 +1,34 @@
-import chartUp from "../../assets/chart-up.svg";
-import chartDown from "../../assets/chart-down.svg";
+import { RotatingLines } from "react-loader-spinner";
 
-function TableCoin({ coins }) {
+import chartUp from "@/assets/chart-up.svg";
+import chartDown from "@/assets/chart-down.svg";
+
+import classes from "./TableCoin.module.css";
+
+function TableCoin({ coins, isLoading }) {
   console.log(coins);
   return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Coin</th>
-            <th>Name</th>
-            <th>Price</th>
-            <th>24h</th>
-            <th>Total Volume</th>
-          </tr>
-        </thead>
-        <tbody>
-          {coins.map((coin) => (
-            <TableRow coin={coin} key={coin.id} />
-          ))}
-        </tbody>
-      </table>
+    <div className={classes.container}>
+      {isLoading ? (
+        <RotatingLines strokeWidth="2" strokeColor="#3874ff" />
+      ) : (
+        <table className={classes.table}>
+          <thead>
+            <tr>
+              <th>Coin</th>
+              <th>Name</th>
+              <th>Price</th>
+              <th>24h</th>
+              <th>Total Volume</th>
+            </tr>
+          </thead>
+          <tbody>
+            {coins.map((coin) => (
+              <TableRow coin={coin} key={coin.id} />
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 }
@@ -28,24 +36,28 @@ function TableCoin({ coins }) {
 export default TableCoin;
 
 const TableRow = ({
-  coin: {image,
-  symbol,
-  name,
-  current_price,
-  total_volume,
-  price_change_percentage_24h: price_change}
+  coin: {
+    image,
+    symbol,
+    name,
+    current_price,
+    total_volume,
+    price_change_percentage_24h: price_change,
+  },
 }) => {
   return (
     <tr>
       <td>
-        <div>
+        <div className={classes.symbol}>
           <img src={image} alt={name} />
           <span>{symbol.toUpperCase()}</span>
         </div>
       </td>
       <td>{name}</td>
       <td>${current_price.toLocaleString()}</td>
-      <td>{price_change.toFixed(2)}</td>
+      <td className={price_change > 0 ? classes.success : classes.error}>
+        {price_change.toFixed(2)}
+      </td>
       <td>{total_volume.toLocaleString()}</td>
       <td>
         <img
--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -4,19 +4,21 @@ import { getCoinList } from "../services/cryptoApi";
 
 function HomePage() {
   const [coins, setCoins] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const getCoins = async() => {
-      const res = await fetch(getCoinList())
-      const json = await res.json()
-      setCoins(json)
-    }
-    getCoins()
+    const getCoins = async () => {
+      const res = await fetch(getCoinList());
+      const json = await res.json();
+      setCoins(json);
+      setIsLoading(false);
+    };
+    getCoins();
   }, []);
 
   return (
     <div>
-      <TableCoin coins={coins} />
+      <TableCoin coins={coins} isLoading={isLoading} />
     </div>
   );
 }
