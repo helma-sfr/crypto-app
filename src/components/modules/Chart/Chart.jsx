f4f3a2115a1d9f0236dd3b47ea089df53029e5f1
helma-sfr <helmasafari76@gmail.com>
2025-05-07 21:22:46 +0330

    worked on Chart

--- src/components/modules/Chart/Chart.jsx
+++ src/components/modules/Chart/Chart.jsx
@@ -1,6 +1,16 @@
 import { convertData } from "@/helpers/ConvertData";
 import classes from "./Chart.module.css";
 import { useState } from "react";
+import {
+  CartesianGrid,
+  LineChart,
+  ResponsiveContainer,
+  Line,
+  Tooltip,
+  Legend,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 function Chart({ chart, setChart }) {
   const [type, setType] = useState("prices");
@@ -10,9 +20,33 @@ function Chart({ chart, setChart }) {
       <span className={classes.cross} onClick={() => setChart(null)}>
         X
       </span>
-      <div className={classes.chart}></div>
+      <div className={classes.chart}>
+        <div className={classes.graph}>
+          <ChartComponent data={convertData(chart, type)} type={type} />
+        </div>
+      </div>
     </div>
   );
 }
 
 export default Chart;
+
+const ChartComponent = ({ data, type }) => {
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <LineChart width={400} height={400} data={data}>
+        <CartesianGrid stroke="#404042" />
+        <Line
+          type="monotone"
+          dataKey={type}
+          stroke="#3874ff"
+          strokeWidth="2px"
+        />
+        <Tooltip />
+        <Legend />
+        <YAxis dataKey={type} domain={["auto", "auto"]} />
+        <XAxis dataKey="date" hide />
+      </LineChart>
+    </ResponsiveContainer>
+  );
+};
